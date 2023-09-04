359c188b2bc7d2a91b3471ecc2f100c94521d354
GTSilver <gtsilver.work@gmail.com>
2023-09-04 17:35:56 +0500

    upload

---
+++ Gradient/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Gradient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start_B = new System.Windows.Forms.Button();
+            this.Result_DGV = new System.Windows.Forms.DataGridView();
+            this.Step_TB = new System.Windows.Forms.TextBox();
+            this.LevelTarget_TB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Regressor_TB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Graphic_PB = new System.Windows.Forms.PictureBox();
+            this.Index_C = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Lot_C = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Level_C = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gradient_C = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quality_C = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Result_DGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Graphic_PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start_B
+            // 
+            this.Start_B.Location = new System.Drawing.Point(296, 12);
+            this.Start_B.Name = "Start_B";
+            this.Start_B.Size = new System.Drawing.Size(188, 20);
+            this.Start_B.TabIndex = 0;
+            this.Start_B.Text = "Calculate";
+            this.Start_B.UseVisualStyleBackColor = true;
+            this.Start_B.Click += new System.EventHandler(this.Start_B_Click);
+            // 
+            // Result_DGV
+            // 
+            this.Result_DGV.AllowUserToAddRows = false;
+            this.Result_DGV.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Result_DGV.BackgroundColor = System.Drawing.Color.White;
+            this.Result_DGV.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.Result_DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Result_DGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Index_C,
+            this.Lot_C,
+            this.Level_C,
+            this.Gradient_C,
+            this.Quality_C,
+            this.Column1});
+            this.Result_DGV.Location = new System.Drawing.Point(12, 38);
+            this.Result_DGV.Name = "Result_DGV";
+            this.Result_DGV.ReadOnly = true;
+            this.Result_DGV.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.Result_DGV.RowHeadersVisible = false;
+            this.Result_DGV.RowHeadersWidth = 20;
+            this.Result_DGV.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.Result_DGV.RowTemplate.Height = 16;
+            this.Result_DGV.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Result_DGV.ShowEditingIcon = false;
+            this.Result_DGV.Size = new System.Drawing.Size(472, 209);
+            this.Result_DGV.TabIndex = 1;
+            // 
+            // Step_TB
+            // 
+            this.Step_TB.Location = new System.Drawing.Point(47, 12);
+            this.Step_TB.Name = "Step_TB";
+            this.Step_TB.Size = new System.Drawing.Size(48, 20);
+            this.Step_TB.TabIndex = 2;
+            this.Step_TB.Text = "10";
+            // 
+            // LevelTarget_TB
+            // 
+            this.LevelTarget_TB.Location = new System.Drawing.Point(127, 12);
+            this.LevelTarget_TB.Name = "LevelTarget_TB";
+            this.LevelTarget_TB.Size = new System.Drawing.Size(48, 20);
+            this.LevelTarget_TB.TabIndex = 4;
+            this.LevelTarget_TB.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "LT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Step";
+            // 
+            // Regressor_TB
+            // 
+            this.Regressor_TB.Location = new System.Drawing.Point(242, 12);
+            this.Regressor_TB.Name = "Regressor_TB";
+            this.Regressor_TB.Size = new System.Drawing.Size(48, 20);
+            this.Regressor_TB.TabIndex = 8;
+            this.Regressor_TB.Text = "2.718281828";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Regressor";
+            // 
+            // Graphic_PB
+            // 
+            this.Graphic_PB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Graphic_PB.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Graphic_PB.Location = new System.Drawing.Point(12, 253);
+            this.Graphic_PB.Name = "Graphic_PB";
+            this.Graphic_PB.Size = new System.Drawing.Size(472, 367);
+            this.Graphic_PB.TabIndex = 14;
+            this.Graphic_PB.TabStop = false;
+            // 
+            // Index_C
+            // 
+            this.Index_C.HeaderText = "№";
+            this.Index_C.Name = "Index_C";
+            this.Index_C.ReadOnly = true;
+            this.Index_C.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Index_C.Width = 32;
+            // 
+            // Lot_C
+            // 
+            this.Lot_C.HeaderText = "Lot";
+            this.Lot_C.Name = "Lot_C";
+            this.Lot_C.ReadOnly = true;
+            this.Lot_C.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Lot_C.Width = 64;
+            // 
+            // Level_C
+            // 
+            this.Level_C.HeaderText = "Level";
+            this.Level_C.Name = "Level_C";
+            this.Level_C.ReadOnly = true;
+            this.Level_C.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Level_C.Width = 64;
+            // 
+            // Gradient_C
+            // 
+            this.Gradient_C.HeaderText = "Step";
+            this.Gradient_C.Name = "Gradient_C";
+            this.Gradient_C.ReadOnly = true;
+            this.Gradient_C.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Gradient_C.Width = 64;
+            // 
+            // Quality_C
+            // 
+            this.Quality_C.HeaderText = "Level Step";
+            this.Quality_C.Name = "Quality_C";
+            this.Quality_C.ReadOnly = true;
+            this.Quality_C.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Quality_C.Width = 88;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 632);
+            this.Controls.Add(this.Graphic_PB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Regressor_TB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LevelTarget_TB);
+            this.Controls.Add(this.Step_TB);
+            this.Controls.Add(this.Result_DGV);
+            this.Controls.Add(this.Start_B);
+            this.MinimumSize = new System.Drawing.Size(512, 160);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Result_DGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Graphic_PB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start_B;
+        private System.Windows.Forms.TextBox Step_TB;
+        private System.Windows.Forms.TextBox LevelTarget_TB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView Result_DGV;
+        private System.Windows.Forms.TextBox Regressor_TB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox Graphic_PB;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Index_C;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lot_C;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Level_C;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gradient_C;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quality_C;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
+
---
+++ Gradient/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace Gradient
+{
+    //https://ru.coursera.org/lecture/machine-learning-foundations/gradiientnyi-spusk-Cws8c
+    //https://www.coursera.org/lecture/machine-learning-foundations/gradiientnyi-spusk-prodolzhieniie-TV3n1
+    //https://www.coursera.org/lecture/machine-learning-foundations/gradiient-i-iegho-svoistva-oaHBN
+
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private class LevelIteration
+        {
+            public double Lot { get; private set; }
+            public double Level { get; private set; }
+
+            public LevelIteration(double lot, Func<double, double> function)
+            {
+                Lot = lot;
+                Level = function(Lot);
+            }
+        }
+
+        private void Start_B_Click(object sender, EventArgs ea)
+        {
+            Result_DGV.Rows.Clear();
+
+            var iteretionList = new List<LevelIteration>();
+            int index = 0;
+            double lot = 0.01;
+            double levelTarget = LevelTarget_TB.Text.ToDouble();
+            double stepLevel = Step_TB.Text.ToDouble();
+            double regressor = Regressor_TB.Text.ToDouble();
+
+            var iterationLast = new LevelIteration(lot, Function);
+
+            iteretionList.Add(iterationLast);
+            Result_DGV.Rows.Add
+            (
+                index,
+                Math.Round(iterationLast.Lot, 2),
+                Math.Round(iterationLast.Level, 0),
+                "-",
+                "-"
+            );
+
+            index++;
+            while (true)
+            {
+                if (index >= 20)
+                    break;
+
+                if (iterationLast.Level <= levelTarget && iterationLast.Level >= 0)
+                    break;
+
+                if (iterationLast.Level >= -levelTarget && iterationLast.Level <= 0)
+                    break;
+
+                if (iteretionList.Count > 1)
+                    if (iterationLast.Lot.Compare(iteretionList[iteretionList.Count - 2].Lot, "=="))
+                        break;
+
+                LevelIteration iterationNew;
+
+                if (iterationLast.Level < 0 && iteretionList.Count > 1)
+                {
+                    if (iteretionList[iteretionList.Count - 2].Level > 0)
+                    {
+                        var iterationBad = iterationLast;
+                        var iterationGood = iteretionList[iteretionList.Count - 2];
+
+                        var l1 = iterationBad.Lot;
+                        var l2 = iterationGood.Lot;
+
+                        var L1 = iterationBad.Level;
+                        var L2 = iterationGood.Level;
+
+                        var DL1 = Math.Abs(levelTarget - L1);
+                        var DL2 = Math.Abs(levelTarget - L2);
+
+                        var lotNew = Math.Round((DL2 * l1 + DL1 * l2) / (DL1 + DL2), 2);
+                        iterationNew = new LevelIteration(lotNew, Function);
+
+                        stepLevel = Math.Round(lotNew - lot, 2, MidpointRounding.AwayFromZero);
+
+                        if (stepLevel < 0.01)
+                            stepLevel = 0.01;
+
+                        lot = lotNew;
+                    }
+                    else
+                    {
+                        stepLevel = Math.Round(stepLevel / regressor, 2, MidpointRounding.AwayFromZero);
+
+                        if (stepLevel < 0.01)
+                            stepLevel = 0.01;
+
+                        iterationNew = new LevelIteration(lot + stepLevel, Function);
+                    }
+                }
+                else if (iterationLast.Level < 0)
+                {
+                    stepLevel = Math.Round(stepLevel / regressor, 2, MidpointRounding.AwayFromZero);
+
+                    if (stepLevel < 0.01)
+                        stepLevel = 0.01;
+
+                    iterationNew = new LevelIteration(lot + stepLevel, Function);
+                }
+                else
+                {
+                    iterationNew = new LevelIteration(lot + stepLevel, Function);
+                    if (iterationNew.Level > 0)
+                        lot = Math.Round(lot + stepLevel, 2, MidpointRounding.AwayFromZero);
+                }
+
+                Result_DGV.Rows.Add
+                (
+                    index,
+                    Math.Round(iterationNew.Lot, 2),
+                    Math.Round(iterationNew.Level, 0),
+                    Math.Round(stepLevel, 2),
+                    Math.Round(iterationNew.Level - iterationLast.Level, 2)
+                );
+
+                iterationLast = iterationNew;
+                iteretionList.Add(iterationLast);
+
+                Graphic(iteretionList, levelTarget);
+                Refresh();
+
+                index++;
+            }
+
+            iteretionList.Add(iterationLast);
+            Graphic(iteretionList, levelTarget);
+        }
+
+        private void Graphic (List<LevelIteration> list, double levelTarget)
+        {
+            var lineGraphic = new Pen(new SolidBrush(Color.Red), 1f);
+            var lineMetric = new Pen(new SolidBrush(Color.White), 1f);
+            var lineZero = new Pen(new SolidBrush(Color.Blue), 1f);
+            var pointGraphic = new Pen(new SolidBrush(Color.Red), 2f);
+            var bmp = new Bitmap(Graphic_PB.Width, Graphic_PB.Height);
+            var graphic = Graphics.FromImage(bmp);
+            var timeStep = 16;
+            var time = 0;
+            var zero = Graphic_PB.Height / 2;
+            var Ymax = list.Max(x => x.Level);
+            var Ymin = list.Min(x => x.Level);
+            var Yabs = 0d;
+
+            if (Math.Abs(Ymax) >= Math.Abs(Ymin))
+                Yabs = Ymax;
+            else
+                Yabs = Ymin;
+
+            var scale = zero / Yabs;
+
+            graphic.DrawLineHorizontal(lineZero, zero);
+            graphic.DrawLineHorizontal(lineMetric, (float)(zero + levelTarget * scale));
+            graphic.DrawLineHorizontal(lineMetric, (float)(zero - levelTarget * scale));
+
+            for (var i = 0; i < list.Count - 1; i++)
+            {
+                var point1 = new Point(time, (int)Math.Round(zero + list[i].Level * scale, 0));
+                var point2 = new Point(time + timeStep, (int)Math.Round(zero + list[i + 1].Level * scale, 0));
+
+                try
+                {
+                    graphic.DrawPoint(pointGraphic, point1, 2f);
+                    graphic.DrawLine(lineGraphic, point1, point2);
+                }
+                catch
+                {
+                    time += timeStep;
+                    continue;
+                }
+
+                time += timeStep;
+            }
+
+            //graphic.DrawPoint(pointGraphic, new Point(time, (int)Math.Round(zero + list[list.Count - 1].Level * scale, 0)), 2f);
+            if (Math.Abs(Ymax) < Math.Abs(Ymin))
+                Graphic_PB.Image = bmp;
+            else
+            {
+                bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                Graphic_PB.Image = bmp;
+            }
+        }
+
+        private double Function(double lot)
+        {
+            var first = random.NextDouble();
+
+            double second;
+            if (random.NextDouble() <= 0.1)
+                second = -1000;
+            else if (random.NextDouble() >= 0.9)
+                second = 1000;
+            else
+                second = 0;
+
+            return 100000 - lot * lot / (lot + lot + 1) * 1000 - lot - lot * lot * lot + lot * lot * first + second;
+        }
+
+        private bool LevelCheck(double limit, LevelIteration iteration)
+        {
+            return iteration.Level <= limit && iteration.Level >= 0;
+        }
+    }
+
+    public static class Extentions
+    {
+        public static int ToInt(this string input)
+        {
+            string result;
+            if (input.Contains(","))
+            {
+                var temporary = input.Split(',');
+                result = temporary[0] + "." + temporary[1];
+            }
+            else
+                result = input;
+            return int.Parse(result, CultureInfo.InvariantCulture);
+        }
+
+        public static short ToShort(this string input)
+        {
+            string result;
+            if (input.Contains(","))
+            {
+                var temporary = input.Split(',');
+                result = temporary[0] + "." + temporary[1];
+            }
+            else
+                result = input;
+            return short.Parse(result, CultureInfo.InvariantCulture);
+        }
+
+        public static byte ToByte(this string input)
+        {
+            string result;
+            if (input.Contains(","))
+            {
+                var temporary = input.Split(',');
+                result = temporary[0] + "." + temporary[1];
+            }
+            else
+                result = input;
+            return byte.Parse(result, CultureInfo.InvariantCulture);
+        }
+
+        public static double ToDouble(this string input)
+        {
+            string result;
+            if (input.Contains(","))
+            {
+                var temporary = input.Split(',');
+                result = temporary[0] + "." + temporary[1];
+            }
+            else
+                result = input;
+            return double.Parse(result, CultureInfo.InvariantCulture);
+        }
+
+        public static float ToFloat(this string input)
+        {
+            string result;
+            if (input.Contains(","))
+            {
+                var temporary = input.Split(',');
+                result = temporary[0] + "." + temporary[1];
+            }
+            else
+                result = input;
+            return float.Parse(result, CultureInfo.InvariantCulture);
+        }
+
+        public static bool Compare(this double a, double b, string type)
+        {
+            switch (type)
+            {
+                case "==": return Math.Abs(a - b) <= double.Epsilon;
+                case "!=": return Math.Abs(a - b) > double.Epsilon;
+                case "<": return a < b - double.Epsilon;
+                case "<=": return a <= b + double.Epsilon;
+                case ">=": return a >= b - double.Epsilon;
+                case ">": return a > b + double.Epsilon;
+                default: throw new InvalidDataException();
+            }
+        }
+
+        public static bool Compare(this float a, float b, string type)
+        {
+            switch (type)
+            {
+                case "==": return Math.Abs(a - b) <= float.Epsilon;
+                case "!=": return Math.Abs(a - b) > float.Epsilon;
+                case "<": return a < b - float.Epsilon;
+                case "<=": return a <= b + float.Epsilon;
+                case ">=": return a >= b - float.Epsilon;
+                case ">": return a > b + float.Epsilon;
+                default: throw new InvalidDataException();
+            }
+        }
+
+        public static void DrawPoint(this Graphics graphic, Pen pen, float X, float Y, float size)
+        {
+            graphic.DrawEllipse(pen, new Rectangle() { X = (int)(X - size / 2), Y = (int)(Y - size / 2), Width = (int)size, Height = (int)size });
+        }
+
+        public static void DrawPoint(this Graphics graphic, Pen pen, Point point, float size)
+        {
+            graphic.DrawEllipse(pen, new Rectangle() { X = (int)(point.X - size / 2), Y = (int)(point.Y - size / 2), Width = (int)size, Height = (int)size });
+        }
+
+        public static void DrawLineHorizontal(this Graphics graphic, Pen pen, float Y)
+        {
+            try
+            {
+                graphic.DrawLine(pen, new Point(0, (int)Y), new Point((int)graphic.ClipBounds.Right, (int)Y));
+            }
+            catch { }
+        }
+
+        public static void DrawLineVerical(this Graphics graphic, Pen pen, float X)
+        {
+            graphic.DrawLine(pen, new Point((int)X, 0), new Point((int)X, (int)graphic.ClipBounds.Bottom));
+        }
+    }
+}
